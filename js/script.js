7482b62db7cad0b2e4b842ca85065491fa639632
evanharmon1 <evanharmon1@gmail.com>
2019-04-19 20:55:54 -0500

    Switch to non-slim jQuery, etc.

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// window.addEventListener("load",function(){
+    
+//     click = document.getElementById("headingOne");
+//     goTo = document.getElementById("collapseOne");
+
+//     click.addEventListener('click', function(){
+//         goTo.scrollIntoView({behavior: "smooth", block: "end"}).visibiltyOfElement(goTo);
+//     });
+// });
